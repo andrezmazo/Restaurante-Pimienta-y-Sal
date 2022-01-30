0447de3b65023b31632f04ee4e9129cfa00cc2d4
unknown <andrez.mazo@gmail.com>
2022-01-30 17:08:09 -0500

    cambios heroku

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.set("view engine", "ejs")
 app.use(express.static("./public"))
 app.use("/", routes)
 
-app.listen(3000, () => {
-    console.log("Servidor corriendo")
-})
+app.listen(process.env.PORT||3000, function() {
+    console.log('Servidor funcionando');
+});
